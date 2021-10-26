b112b1ff2b1f1c335cbef4bbf914e379c1bcae21
Felipe <felipe.escorel@gmail.com>
2021-10-25 23:48:31 -0300

    experiment with go routines and mutex :bug::bug:

--- main.go
+++ main.go
@@ -10,5 +10,7 @@ func main() {
 	fmt.Println("Scanneando Porta")
 	results := port.InitialScan("localhost")
 	fmt.Println(results)
-
+	fmt.Println("\n============================\n")
+	wideScanResults := port.WideScan("localhost")
+	fmt.Println(wideScanResults)
 }
--- port/port.go
+++ port/port.go
@@ -1,14 +1,24 @@
 package port
 
 import (
+	"fmt"
 	"net"
+	"runtime"
 	"strconv"
+	"sync"
 	"time"
 )
+
+var mu sync.Mutex
+var wg sync.WaitGroup
 
 type ScanResult struct {
 	Port  int
 	State string
+}
+
+func Rotinas(start int, end int) {
+
 }
 
 func ScanPort(protocol, hostname string, port int) ScanResult {
@@ -26,30 +36,58 @@ func ScanPort(protocol, hostname string, port int) ScanResult {
 }
 
 func InitialScan(hostname string) []ScanResult {
-
+	start := time.Now()
 	var results []ScanResult
-
-	for i := 0; i <= 1024; i++ {
-		results = append(results, ScanPort("tcp", hostname, i))
+	var result ScanResult
+	i := 0
+	portRange := 1024
+	routinesCount := runtime.NumCPU()
+	indexRange := portRange / routinesCount
+	wg.Add(routinesCount)
+	for j := 1; j <= routinesCount; j++ {
+		go func() {
+			currentIndex := j
+			for i < indexRange*currentIndex {
+				mu.Lock()
+				result = ScanPort("tcp", hostname, i)
+				i++
+				mu.Unlock()
+				if result.State == "Open" {
+					results = append(results, result)
+				}
+			}
+			wg.Done()
+		}()
 	}
 
-	for i := 0; i <= 1024; i++ {
-		results = append(results, ScanPort("udp", hostname, i))
-	}
-
+	wg.Wait()
+	// for i := 0; i <= 1024; i++ {
+	// 	results = append(results, ScanPort("udp", hostname, i))
+	// }
+	fmt.Println("initial scan com rotinas ", time.Since(start))
 	return results
 }
 
 func WideScan(hostname string) []ScanResult {
+	fmt.Println("wide scan start $$$$$$")
+	start := time.Now()
+
 	var results []ScanResult
 
-	for i := 0; i <= 49152; i++ {
-		results = append(results, ScanPort("udp", hostname, i))
-	}
+	// for i := 0; i <= 49152; i++ {
+	// 	go ScanPort("udp", hostname, i)
+
+	// 	results = append(results)
+	// }
 
 	for i := 0; i <= 49152; i++ {
-		results = append(results, ScanPort("tcp", hostname, i))
+		fmt.Printf("olalala %d \n", i)
+		result := ScanPort("tcp", hostname, i)
+		if result.State == "Open" {
+			results = append(results, result)
+		}
 	}
+	fmt.Println("WideScan sem rotinas ", time.Since(start))
 
 	return results
 }
