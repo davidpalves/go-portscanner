83c66d2cd5c1de90eb86346e179d706ee9bd9e0c
Felipe <felipe.escorel@gmail.com>
2021-10-25 21:16:32 -0300

    adds main function :bug:

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func scanPort(protocol, hostname string, port int) bool {
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DialTimeout(protocol, address, 60*time.Second)
+
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
+
+func main() {
+	fmt.Println("Scanneando Porta")
+	open := scanPort("tcp", "localhost", 8080)
+
+	fmt.Printf("porta aberta: %t\n", open)
+
+}
